e4f98b4a6ceb98751689a0ef83d1221821145105
Munteanu <munteanuvictoria1@gmail.com>
2024-02-28 21:02:32 +0100

    first commit 🎁

---
+++ generator_quotes/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css'; 
+import axios from 'axios'; 
+
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div className="App">
+                <h1> Hello Greenish</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
